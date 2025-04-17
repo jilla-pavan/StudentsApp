fa46a90209c9193e8a969fdae6130c934b0b96fe
jilla-pavan <jillapavan22001@gmail.com>
2025-04-17 13:07:35 +0530

    changed the email

--- frontend/src/components/views/StudentProgressReport.jsx
+++ frontend/src/components/views/StudentProgressReport.jsx
@@ -1096,7 +1096,7 @@ const StudentProgressReport = ({ students, batches }) => {
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-3 text-gray-500 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                                         </svg>
-                                        <span className="text-gray-700 break-all">careersureacademypayment@gmail.com</span>
+                                        <span className="text-gray-700 break-all">careersure.info@gmail.com</span>
                                     </div>
                                 </div>
                             </div>
